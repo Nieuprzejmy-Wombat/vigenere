24ea12116a09e7efbc775ca08120c9f049422368
wombat <u58_makcza_waw@technischools.com>
2024-01-09 15:48:17 +0100

    wrote table

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Lib
+import Lib (table)
 
 main :: IO ()
-main = someFunc
+main = print table
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,10 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (table) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char (chr, ord)
+
+-- 97 - 122
+table :: [[Char]]
+table = [shift n <$> ['a' .. 'z'] | n <- [0 .. 25]]
+ where
+  shift :: Int -> Char -> Char
+  shift n c = chr $ (((ord c - 97) + n) `mod` 26) + 97
