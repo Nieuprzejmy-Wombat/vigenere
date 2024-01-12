d37f5d0965a39cda6e482abc3bcc27aa71b5d452
wombat <u58_makcza_waw@technischools.com>
2024-01-12 12:46:15 +0100

    ready but with warnings

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,17 +8,19 @@ table = [shift n <$> ['a' .. 'z'] | n <- [0 .. 25]]
   shift :: Int -> Char -> Char
   shift n c = chr $ (((ord c - 97) + n) `mod` 26) + 97
 
--- matchLength n cs repeats cs until its length is equal to n
-matchLength :: Int -> String -> String
-matchLength n cs = case compare n (length cs) of
-  GT -> matchLength n $ cs <> cs
-  EQ -> cs
-  LT -> take n cs
+-- make key equal in length to message by repeating it and copying special characters
+matchLength :: String -> String -> String
+matchLength ks cs = matchLength' ks cs 0
+  where
+    matchLength' ks' cs' index
+      | null cs' = ""
+      | not . isAsciiLower . head $ cs' = head cs' : matchLength' ks' (tail cs') index
+      | otherwise = ks' !! index : matchLength' ks' (tail cs') (if index + 1 == length ks' then 0 else index + 1) 
+
 
 encrypt :: String -> String -> String
-encrypt ks cs = (<>)
+encrypt ks cs = zipWith encryptChar (matchLength ks cs) cs
   where
     encryptChar k c = if isAsciiLower c then table !! (ord k - 97) !! (ord c - 97) else c
-    ks' = matchLength (length cs) ks
 
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Lib
+import Data.Bool
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn $ bool "testSpecialChars not passed" "testSpecialChars passed" testSpecialChars
+
+testSpecialChars :: Bool
+testSpecialChars = encrypt "tajne" "to jest bardzo tajny tekst" == "mo srwm bjehso cnngy crolt"
+
+
