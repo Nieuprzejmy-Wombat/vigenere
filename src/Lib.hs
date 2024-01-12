23a88dc4198f9383ef8d122403edd986c633fbe7
wombat <u58_makcza_waw@technischools.com>
2024-01-12 21:21:23 +0100

    made matchLength prettier

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,21 +6,22 @@ table :: [[Char]]
 table = [shift n <$> ['a' .. 'z'] | n <- [0 .. 25]]
  where
   shift :: Int -> Char -> Char
-  shift n c = chr $ (((ord c - 97) + n) `mod` 26) + 97
+  shift n = chr . (+ 97) . (`mod` 26) . (+ n) . (flip (-) 97) . ord
 
--- make key equal in length to message by repeating it and copying special characters
 matchLength :: String -> String -> String
 matchLength ks cs = matchLength' ks cs 0
   where
-    matchLength' ks' cs' index
-      | null cs' = ""
-      | not . isAsciiLower . head $ cs' = head cs' : matchLength' ks' (tail cs') index
-      | otherwise = ks' !! index : matchLength' ks' (tail cs') (if index + 1 == length ks' then 0 else index + 1) 
-
+    matchLength' _ "" _ = ""
+    matchLength' ks' (c':cs') index = if isAsciiLower c' 
+      then ks' !! index : matchLength' ks' cs' ((index + 1) `mod` length ks')
+      else c' : matchLength' ks' cs' index
 
 encrypt :: String -> String -> String
 encrypt ks cs = zipWith encryptChar (matchLength ks cs) cs
   where
     encryptChar k c = if isAsciiLower c then table !! (ord k - 97) !! (ord c - 97) else c
 
+decrypt :: String -> String -> String
+decrypt ks cs = encrypt ks' cs
+  where ks' = chr . (+ 97) . (`mod` 26) . (26 -) . (flip (-) 97) . ord <$> ks
 
